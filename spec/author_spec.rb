5fbb16861bb09ed6165228fd0646e1b737401f70
pcbennett108 <pcbennett42@gmail.com>
2023-05-31 16:56:05 -0600

    Add author spec

---
+++ spec/author_spec.rb
@@ -0,0 +1,28 @@
+require 'rspec'
+require './lib/book'
+require './lib/author'
+
+RSpec.describe Author do
+  describe "Iteration 1" do
+    it "exists and has attributes" do
+      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+      expect(charlotte_bronte).to be_a Author
+      expect(charlotte_bronte.name).to eq("Charlotte Bronte")
+      expect(charlotte_bronte.books).to eq([])
+    end
+
+    it "can write books" do
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+      expect(jane_eyre.class).to eq(Book)
+      expect(jane_eyre.title).to eq("Jane Eyre")
+      
+      villette = charlotte_bronte.write("Villette", "1853")
+      
+      expect(charlotte_bronte.books).to eq([jane_eyre, villette])
+    end
+
+
+  end
+end
