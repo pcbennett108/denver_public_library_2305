e8e00d5ff08dd962d317256a57ad3e193ab02b2d
pcbennett108 <pcbennett42@gmail.com>
2023-05-31 22:19:30 -0600

    Add tests for inventory

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -65,10 +65,27 @@ RSpec.describe Library do
       dpl.return(mockingbird)
       dpl.return(villette)
       expect(dpl.checked_out_books).to eq([jane_eyre])
+    end
+  end
+
+  describe "Iteration 4" do
+    it "takes inventory and finds most popular book" do
+      dpl = Library.new("Denver Public Library")
+      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      professor = charlotte_bronte.write("The Professor", "1857")
+      villette = charlotte_bronte.write("Villette", "1853")
+      harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+      mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+      dpl.add_author(charlotte_bronte)
+      dpl.add_author(harper_lee)
+
+      watchman = harper_lee.write("Go Set a Watchman", "July 14, 2015")
+
+      expect(dpl.inventory).to eq({charlotte_bronte => [jane_eyre, professor, villette], harper_lee => [mockingbird, watchman]})
 
     end
-
-
   end
 
 end
