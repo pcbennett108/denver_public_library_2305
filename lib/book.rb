22a9318b40a8d483ed9ff78256622276b0d969df
pcbennett108 <pcbennett42@gmail.com>
2023-06-01 21:42:14 -0600

    Add method for most_popular_book & refactor inventory method

--- lib/book.rb
+++ lib/book.rb
@@ -1,15 +1,22 @@
 class Book
-  attr_reader :title, :publication_year
+  attr_reader :title,
+              :publication_year,
+              :times_checked_out
 
   def initialize(info)
     @author_first_name = info[:author_first_name]
     @author_last_name = info[:author_last_name]
     @title = info[:title]
     @publication_year = info[:publication_date].split.last
+    @times_checked_out = 0
   end
 
   def author
     "#{@author_first_name} #{@author_last_name}"
+  end
+
+  def add_checkout
+    @times_checked_out += 1
   end
 
   
